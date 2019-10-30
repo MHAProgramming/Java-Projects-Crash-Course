d9464b97c4cfea9545ae51b13c0d0a3def60c9f6
Haroun <mharounazizi@gmail.com>
2019-10-30 11:29:23 -0400

    Added Notes to the Eliza Main File

--- Eliza/src/academy/learnprogramming/Main.java
+++ Eliza/src/academy/learnprogramming/Main.java
@@ -2,10 +2,14 @@ package academy.learnprogramming;
 
 import java.util.Scanner;
 
+//this is the main Class that holds everything
+
 public class Main {
 
     public static void main(String[] args) {
 	    var scanner = new Scanner(System.in);
+
+	    // This line prints out the intro to the Doctor.java program
 
 	    System.out.println(Doctor.intro());
 	    var userInput = "";
